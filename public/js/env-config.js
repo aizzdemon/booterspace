3bb5a0ba322b30da1cb454d46253cb1c71ebb545
Aizz Symbol <aizzromio@gmail.com>
2026-03-12 17:18:37 +0530

    fix: restore firebase defaults so pages load data

--- public/js/env-config.js
+++ public/js/env-config.js
@@ -1,4 +1,19 @@
 (function attachEnvReader(globalScope) {
+  const defaultFirebaseEnv = {
+    VITE_FIREBASE_API_KEY: "AIzaSyBeGZBE1u1-y1hDWbRouchgwkgp89D973I",
+    VITE_FIREBASE_AUTH_DOMAIN: "kar-kardan.firebaseapp.com",
+    VITE_FIREBASE_PROJECT_ID: "kar-kardan",
+    VITE_FIREBASE_STORAGE_BUCKET: "kar-kardan.firebasestorage.app",
+    VITE_FIREBASE_MESSAGING_SENDER_ID: "554147696994",
+    VITE_FIREBASE_APP_ID: "1:554147696994:web:221dcb883e3b65dcea5c3b",
+    VITE_FIREBASE_MEASUREMENT_ID: "G-RRC3X485KQ"
+  };
+
+  globalScope.__ENV__ = {
+    ...defaultFirebaseEnv,
+    ...globalScope.__ENV__
+  };
+
   if (typeof globalScope.getEnvVar === "function") return;
 
   globalScope.getEnvVar = function getEnvVar(...keys) {
